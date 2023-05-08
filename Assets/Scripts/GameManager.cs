d464485e0d695f964fabeaa8866cfce8921bebae
fujimiyanatsuki <fujimiyanatsuki@gmail.com>
2023-05-08 23:26:34 +0900

    いろいろ修正しました。

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,21 @@
+using System.Collections;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
 public class GameManager : SingletonBase<GameManager>
 {
+    /// <summary>
+    /// Awake
+    /// </summary>
     protected override void Awake()
     {
         base.Awake();
     }
 
+    /// <summary>
+    /// Start
+    /// </summary>
     void Start()
     {
         InitializeGame();
@@ -15,10 +26,20 @@ public class GameManager : SingletonBase<GameManager>
     /// </summary>
     public void InitializeGame()
     {
+        PanelManager.Instance.Initialize();
         BlockManager.Instance.Initialize();
         BlockCounter.Instance.Initialize(BlockManager.Instance.GetAllBlockCount());
         BarManager.Instance.Initialize();
+        StartCoroutine(WaitForBlocksAndInitializeBall());
+    }
+
+    /// <summary>
+    /// Blockの整列が完了するまで待機
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator WaitForBlocksAndInitializeBall()
+    {
+        yield return new WaitForSeconds(BlockManager.Instance.TotalLayoutTime());
         BallManager.Instance.Initialize();
-        PanelManager.Instance.Initialize();
     }
 }
